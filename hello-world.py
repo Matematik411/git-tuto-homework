4f799ddd6a3e4d9501a5d37ebd590e644a219a56
Nejc Zajc <49908468+Matematik411@users.noreply.github.com>
2025-08-31 21:41:05 +0200

    feat: Ground Control

--- hello-world.py
+++ hello-world.py
@@ -2,4 +2,8 @@ print("Hello world!")
 
 print("This is the world responding.")
 
+print("Commencing countdown, engines on.")
+
 print("A Martian is also here saying 'Howdy!'")
+
+print("I'm stepping through the door.\nAnd I'm floating in a most peculiar way.")
