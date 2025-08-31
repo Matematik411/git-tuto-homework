c1ea79f52c19471d8ff2f0bf9f7b4a65c7efc8d3
Nejc Zajc <49908468+Matematik411@users.noreply.github.com>
2025-08-31 21:43:47 +0200

    feat: more message updates

--- hello-world.py
+++ hello-world.py
@@ -4,4 +4,4 @@ print("This is the World responding.")
 
 print("Message sent successfully.")
 
-print("A Martian is also here saying 'Howdy!'")
+print("Message 'Message sent successfully' has been received.")
