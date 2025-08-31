e9c1f50a4121c6f830f4f5e742e257da7d4e26a1
Nejc Zajc <49908468+Matematik411@users.noreply.github.com>
2025-08-31 21:47:39 +0200

    feat: more messages, more

--- hello-world.py
+++ hello-world.py
@@ -5,3 +5,5 @@ print("This is the World responding.")
 print("Message sent successfully.")
 
 print("Message 'Message sent successfully' has been received.")
+
+print("More and more and more news here!")
